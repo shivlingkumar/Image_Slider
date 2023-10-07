b4575ee05d2f6a23d4007e624f114c4cab346cdc
shivlingkumar <shivlingkumar295@gmail.com>
2023-10-07 23:20:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/image_slider/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.image_slider
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.image_slider.R
+import com.example.image_slider.adapter.ImageSliderAdapter
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager2
+    private val images = listOf(
+        "https://phppot.com/wp-content/uploads/2015/07/jquwey-image-carousal.png",
+        "https://www.adobe.com/acrobat/hub/media_173d13651460eb7e12c0ef4cf8410e0960a20f0ee.jpeg?width=1200&format=pjpg&optimize=medium",
+        "https://media.istockphoto.com/id/1185953092/photo/the-main-attraction-of-paris-and-all-of-europe-is-the-eiffel-tower-in-the-rays-of-the-setting.jpg?s=612x612&w=0&k=20&c=ZneUFVlR5TPCooxRV1rldUAadqLxZEvOoyVCOrtpcRQ=",
+        "https://imgv3.fotor.com/images/cover-photo-image/a-beautiful-girl-with-gray-hair-and-lucxy-neckless-generated-by-Fotor-AI.jpg",
+        "https://www.jqueryscript.net/images/Awesome-Fully-Responsive-jQuery-Slider-FlexSlider.jpg",
+         "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+        "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        val adapter = ImageSliderAdapter(images)
+        viewPager.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/example/image_slider/adapter/ImageSliderAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.image_slider.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+import com.example.image_slider.R
+
+class ImageSliderAdapter(private val images: List<String>) :
+    RecyclerView.Adapter<ImageSliderAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_image_slider, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageUrl = images[position]
+        Glide.with(holder.itemView)
+            .load(imageUrl)
+            .into(holder.imageView)
+    }
+}
